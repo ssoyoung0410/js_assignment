80f3215c8fc582dcdece2d44215a5e7487e40f38
Lee So Young <cheerup0101@naver.com>
2019-10-30 23:50:49 +0900

    test

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
-// TODO 투두 코맨트 테스트
+/ TODO 투두 코맨트 테스트
 const main = (...args) => {
     args.forEach(e => {
         console.log(e);
     });
-}
-// FIXME 픽스미 코맨트 테스트
+} 
+/ FIXME 픽스미 코맨트 테스트
 main('test1', 'test2', 'test3');
+
+/뭐지!!!? 
