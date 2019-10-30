d9d9e385338aa32f0fed42104cc15b4bc007c24e
문한주(163229) <luvet@shinsegae.com>
2019-10-30 23:54:42 +0900

    테스트 코맨트 XXX 샘플

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
-/ TODO 투두 코맨트 테스트
+// XXX 확인요망 코맨트 테스트
 const main = (...args) => {
     args.forEach(e => {
         console.log(e);
     });
 } 
-/ FIXME 픽스미 코맨트 테스트
-main('test1', 'test2', 'test3');
 
-/뭐지!!!? 
+main('test1', 'test2', 'test3');
