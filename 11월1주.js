500635a410ab97a068049b3cc726b58e5263235f
Lee So Young <cheerup0101@naver.com>
2019-11-08 19:47:00 +0900

    API 데이터 사용하기- 수정

--- 11월1주.js
+++ 11월1주.js
@@ -18,15 +18,14 @@
 // render(dataList2);
 // render(dataList3);
 // render(dataList4);
-
-// TODO API에서 받아온 데이터를 사용하도록 수정 해보세요!
 axios('https://my-json-server.typicode.com/it-crafts/mockapi/timeline?_page=1')
 .then(function(list) {
-    console.log(list.data);
     const app = document.getElementById('app');
     const render = function(list) {
+        //console.log(list); 
         let img = '';
         list.forEach(data => {
+            //console.log(data);
             img += `<div class="Nnq7C weEfm"><div class="v1Nh3 kIKUG _bz0w"><a href="javascript:;"><div class="eLAPa">
             <div class="KL4Bh"><img class="FFVAD" decoding="auto" src="${data.src1}" style="object-fit: cover;"></div>
             <div class="_9AhH0"></div></div><div class="u7YqG"><span aria-label="슬라이드" class="mediatypesSpriteCarousel__filled__32 u-__7"></span></div></a></div><div class="v1Nh3 kIKUG _bz0w"><a href="javascript:;">
@@ -45,11 +44,12 @@ render(list.data);
 
 axios('https://my-json-server.typicode.com/it-crafts/mockapi/timeline?_page=2')
 .then(function(list) {
-  console.log(list.data);
   const app = document.getElementById('app');
   const render2 = function(list) {
       let img = '';
+      console.log(list);
       list.forEach(data => {
+          //console.log(data);
           img += `<div class="Nnq7C weEfm"><div class="v1Nh3 kIKUG _bz0w"><a href="javascript:;"><div class="eLAPa">
           <div class="KL4Bh"><img class="FFVAD" decoding="auto" src="${data.src1}" style="object-fit: cover;"></div>
           <div class="_9AhH0"></div></div><div class="u7YqG"><span aria-label="슬라이드" class="mediatypesSpriteCarousel__filled__32 u-__7"></span></div></a></div><div class="v1Nh3 kIKUG _bz0w"><a href="javascript:;">
